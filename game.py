b3a589b4023a988e35fffc35e94d46fb36839c10
bernadette breboneria <bernadette.breboneria@biarri.com>
2020-01-23 16:08:44 +0800

    added while loop

--- game.py
+++ game.py
@@ -1,29 +1,21 @@
-
-player_name = 'Manuel'
-player_attack = 10
-player_heal = 16
-health = 100
- 
 # dictionaries
-player = {'name': 'Manuel', 'attack': 10, 'heal': 16, 'health':100}
-monster = {'name': 'Max', 'attack': 12,'health':100}
+player = {'name': 'Manuel', 'attack': 10, 'heal': 16, 'health': 100}
+monster = {'name': 'Max', 'attack': 12, 'health': 100}
+game_running = True
 
-print('Please select action')
-print('1) Attack')
-print('2) Heal')
-player_choice = input()
+while game_running == True:
+    print('Please select action')
+    print('1) Attack')
+    print('2) Heal')
+    player_choice = input()
 
+    if player_choice == 1:
+        monster['health'] = monster['health'] - player['attack']
+        player['health'] = player['health'] - monster['attack']
 
-
-if player_choice == 1:
-     monster['health'] = monster['health'] - player['attack']
-     player['health'] = player['health'] - monster['attack']
-     
-     print(monster['health'])
-     print(player['health'])
-elif  player_choice == 2:
-    print('Heal player')
-else: print('Invalid Input')
-
-
-
+        print(monster['health'])
+        print(player['health'])
+    elif player_choice == 2:
+        print('Heal player')
+    else:
+        print('Invalid Input')
